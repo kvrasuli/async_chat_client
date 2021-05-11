8c09d20d5b1cd97de8d6c714aed0cb2d827ce1db
Kirill Rasuli <rasulikv@gmail.com>
2021-05-11 19:49:46 +0300

    Rework exception handling

--- readfromchat.py
+++ readfromchat.py
@@ -14,24 +14,25 @@ async def open_socket(host, port):
     try:
         reader, writer = await asyncio.open_connection(host, port)
         yield reader, writer
-    except ConnectionError:
-        logger.error('Reading error!')
-        await asyncio.sleep(10)
     finally:
         writer.close()
         await writer.wait_closed()
 
 
 async def get_messages_from_chat(host, port, path):
-    async with open_socket(host, port) as stream:
-        while True:
-            reader, _ = stream[0], stream[1]
-            chat_line = await reader.readline()
-            timestamp = datetime.datetime.now().strftime('%d.%m.%y %H:%M:%S')
-            chat_line_with_timestamp = f'[{timestamp}] {chat_line.decode()}'
-            print(chat_line_with_timestamp, end='')
-            async with aiofiles.open(path, 'a', encoding='utf-8') as file:
-                await file.write(chat_line_with_timestamp)
+    try:
+        async with open_socket(host, port) as stream:
+            while True:
+                reader, _ = stream[0], stream[1]
+                chat_line = await reader.readline()
+                timestamp = datetime.datetime.now().strftime('%d.%m.%y %H:%M:%S')
+                chat_line_with_timestamp = f'[{timestamp}] {chat_line.decode()}'
+                print(chat_line_with_timestamp, end='')
+                async with aiofiles.open(path, 'a', encoding='utf-8') as file:
+                    await file.write(chat_line_with_timestamp)
+    except ConnectionError:
+        logger.error('Reading error!')
+        await asyncio.sleep(10)
 
 
 def parse_args():
